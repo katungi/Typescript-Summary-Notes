bce3f448d7cfef1a16bef0b036e82a0a515bac65
katungi <dankatdennis@gmail.com>
2022-01-24 10:23:03 +0300

    - added maps setup

--- projects/maps/src/index.ts
+++ projects/maps/src/index.ts
@@ -1,1 +1,6 @@
-console.log('Hello world');
+import {User} from './User';
+import {Company } from './Company';
+
+const user = new User();
+const company = new Company();
+console.log(user, company);
